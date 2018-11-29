89de5d91e363a79a7b39deaf7a206b933ef094cf
peerajak <peerajak@gmail.com>
2018-11-29 13:55:51 +0700

    add post user with authentication

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env****',env);
+if(env === 'development'){
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';//My server
+}else{
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';//My Test server
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('./config/config.js')
+require('./config/config.js');
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -105,12 +105,17 @@ app.patch('/todos/:id', (req,res) =>{
 
 //POST /users
 app.post('/users', (req,res)=>{
+  var body = _.pick(req.body,['email','password'])
   var user = new User({
-    text: req.body.text
+    email: body.email,
+    password: body.password
   })
-  todo.save().then((doc)=>{
-    res.status(200).send(doc);
-  },(e)=>{
+  user.save().then(()=>{
+    //res.status(200).send(user);
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
     res.status(400).send(e);
   });
 });
