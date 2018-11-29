a8a72b3ae35115b271d528c75a935d68fd5a9440
peerajak <peerajak@gmail.com>
2018-11-29 11:03:00 +0700

    end of Chapter 7 Mangoose

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config.js')
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -52,6 +54,55 @@ app.get('/todos/:id', (req,res)=>{
   });
 });
 
+app.delete('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+    console.log('todos/:id');
+  if(!ObjectID.isValid(id)){
+    console.log('object id invalid');
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      console.log('object to delete id not found');
+      return res.status(404).send();
+    }
+    res.send({todo});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+app.patch('/todos/:id', (req,res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']) // if text,completed key exist, update them
+  console.log('todos/:id');
+  if(!ObjectID.isValid(id)){
+    console.log('object id invalid');
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    if(!todo){
+      console.log('object to delete id not found');
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+});
+
+
+
 //POST /users
 app.post('/users', (req,res)=>{
   var user = new User({
@@ -66,7 +117,7 @@ app.post('/users', (req,res)=>{
 
 
 app.listen(port, ()=>{
-  console.log('Started on port ${port}');
+  console.log(`Started on port ${port}`);
 })
 
 
